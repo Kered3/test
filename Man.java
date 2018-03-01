c32c88005c36a58cd05a0eae0765f89fe07f66c5
Kered3 <34865083+Kered3@users.noreply.github.com>
2018-03-01 11:08:09 -0500

    add

---
+++ Man.java
@@ -0,0 +1,11 @@
+public class Man {
+
+    private final int age;
+
+    private final String name;
+
+    public Man(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+}
